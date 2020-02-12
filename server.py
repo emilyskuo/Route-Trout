c9f1ab452891a7ba5f6dd0a0b7c7c06e60a34fd0
Emily Kuo <em.s.kuo@gmail.com>
2020-02-12 12:32:02 -0800

    added initial routes for homepage and registration page

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import (Flask, render_template, redirect, request, flash, session)
-from flask_sqlalchemy import flask_sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
 from jinja2 import StrictUndefined
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -12,5 +12,27 @@ app.secret_key = "supersecret"
 # For getting error messages in Jinja when variables are undefined
 app.jinja_env.undefined = StrictUndefined
 
+@app.route("/")
+def index():
+    """Display homepage"""
+
+    return render_template("index.html")
+
+@app.route("/register")
+def reg_form():
+    """Display registration form"""
+
+    return render_template("register.html")
+
+# @app.route("/register", methods=["POST"])
+# def register_user():
+#     """Creates new user if user does not yet exist"""
+
+#     email = request.form.get("email")
+#     password = request.form.get("password")
+
+
+#     """validate if username is taken, email is used, cell already exists"""
+#     if not User.query.filter_by(email=email).first():
 
 
