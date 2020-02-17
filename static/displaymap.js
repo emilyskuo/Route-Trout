3f5649594490c38d94968bcc9a3f82bca1735e5a
Emily Kuo <em.s.kuo@gmail.com>
2020-02-17 11:23:05 -0800

    updated initMap function to create map & marker within ajax callback function

--- static/displaymap.js
+++ static/displaymap.js
@@ -5,42 +5,16 @@ const trail_name = $("#trail-name").html();
 console.log(trail_name);
 
 function initMap() {
-    const map = new google.maps.Map(
-        document.getElementById("map-container"), {
-            center: {
-                lat: 37.601773, 
-                lng: -122.202870
-            },
-            zoom: 11,
-        },
-    );
-    
     $.get(`/json/latlongbyid/${trail_name}`, (res) => {
-        map.setCenter(res);
-        console.log(res);
+        const map = new google.maps.Map(
+            document.querySelector("#map-container"), {
+                center: res,
+                zoom: 15,
+            },
+        );
         const newMarker = new google.maps.Marker({
             position: res,
             map: map
         });
     });
-}
-
-
-// const marker = new google.maps.Marker(
-//     {position: current_trail, 
-//     map: map
-//     }
-// );
-// }
-
-// console.log(current_trail_jquery);
-
-// console.log(current_trail_jquery["responseJSON"]);
-
-// console.log(current_trail_jquery.responseJSON);
-
-// const current_trail = current_trail_jquery["responseJSON"];
-
-// console.log(current_trail);
-
-
+};
