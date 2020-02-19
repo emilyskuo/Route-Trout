f4abcdd42980748803877acb7f6f749980bf4845
Emily Kuo <em.s.kuo@gmail.com>
2020-02-18 16:46:53 -0800

    displays search results as a list and each location marked on map

--- static/searchresults.js
+++ static/searchresults.js
@@ -1,31 +1,49 @@
 "use strict";
 
-const fourPassLoop = {
-    lat: 39.0985,
-    lng: -106.9407
-};
+// const fourPassLoop = {
+//     lat: 39.0985,
+//     lng: -106.9407
+// };
 
-function initMap() {
-    const map = new google.maps.Map(
-        document.getElementById("map-container"), 
-        {zoom: 10, 
-        center: fourPassLoop
-        }
-    );
-    const marker = new google.maps.Marker(
-        {position: fourPassLoop, 
-         map: map
-        }
-    );
-}
+// function initMap() {
+//     const map = new google.maps.Map(
+//         document.getElementById("map-container"), 
+//         {zoom: 10, 
+//         center: fourPassLoop
+//         }
+//     );
+//     const marker = new google.maps.Marker(
+//         {position: fourPassLoop, 
+//          map: map
+//         }
+//     );
+// }
 
 const params = (new URL(document.location)).searchParams;
 const search = params.get("search");
 
-$.get(`/json/search`, {search: search}, (res) => {
-    console.log(res);
-    for (const trail of res) {
-        console.log(trail.name);
-        $("#trail-list").append(`<li><a href="/trail/${trail.id}">${trail.name}</li>`);
-    }
-});
+function initMap() {
+    $.get(`/json/search-coords`, {search: search}, (res) => {
+        const map = new google.maps.Map(
+            document.querySelector("#map-container"),
+            {zoom: 8,
+            center: res
+            }
+        );
+        $.get(`/json/search`, {search: search}, (res2) => {
+            console.log(res2);
+            for (const trail of res2) {
+                console.log(trail.name);
+                $("#trail-list").append(`<li><a href="/trail/${trail.id}">${trail.name}</li>`);
+                const marker = new google.maps.Marker(
+                    {position: {
+                        lng: Number(trail.longitude),
+                        lat: Number(trail.latitude)
+                    },
+                    map: map
+                    }
+                );
+            }
+        });
+    });
+}
