bcf124eea6c37ccb4b8ee7153cf30a6be2266765
Emily Kuo <em.s.kuo@gmail.com>
2020-02-27 14:43:28 -0800

    added icon for "home" and updated "home" marker with info window

--- static/displaytripmap.js
+++ static/displaytripmap.js
@@ -6,17 +6,26 @@ const tripPath = window.location.pathname
 // Slice pathname string to just include the trail_id
 const trip_id = tripPath.slice(6)
 
+const houseIcon = "/static/images/house.png"
+
 function initMap() {
-    $.get(`/json/tripcoords`, {trip_id: trip_id}, (res) => {
+    $.get(`/json/tripinfo`, {trip_id: trip_id}, (res) => {
         const map = new google.maps.Map(
             document.querySelector("#tripmapdiv"), {
-                center: res,
+                center: res.lat_long,
                 zoom: 15,
             },
         );
         const newMarker = new google.maps.Marker({
-            position: res,
-            map: map
+            position: res.lat_long,
+            map: map,
+            icon: houseIcon
+        });
+        const infowindow = new google.maps.InfoWindow({
+            content: `<b>Trip Accommodations:</b> ${res.accom_text}`,
+        });
+        newMarker.addListener("click", () => {
+            infowindow.open(newMarker.get("map"), newMarker);
         });
     });
 };
