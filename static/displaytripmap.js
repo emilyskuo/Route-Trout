53b76d3c81e38987fd23ac7ec09f8596df37f9f9
Emily Kuo <em.s.kuo@gmail.com>
2020-02-27 12:52:59 -0800

    created new js file for display trip map - copied displaytrailmap code in comments for reference

---
+++ static/displaytripmap.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// // Grab pathname from browser
+// const path = window.location.pathname
+
+// // Slice pathname string to just include the trail_id
+// const trail_id = path.slice(7)
+
+// function initMap() {
+//     $.get(`/json/latlongbyid/${trail_id}`, (res) => {
+//         const map = new google.maps.Map(
+//             document.querySelector("#map-container"), {
+//                 center: res,
+//                 zoom: 15,
+//             },
+//         );
+//         const newMarker = new google.maps.Marker({
+//             position: res,
+//             map: map
+//         });
+//     });
+// };
