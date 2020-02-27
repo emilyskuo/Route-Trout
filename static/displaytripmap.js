102bed1ee865f70cf7b80ed0fd172578dd8440d0
Emily Kuo <em.s.kuo@gmail.com>
2020-02-27 12:59:58 -0800

    set variables to grab trip_id value

--- static/displaytripmap.js
+++ static/displaytripmap.js
@@ -1,10 +1,13 @@
 "use strict";
 
-// // Grab pathname from browser
-// const path = window.location.pathname
+// Grab pathname from browser
+const tripPath = window.location.pathname
+console.log(tripPath)
 
-// // Slice pathname string to just include the trail_id
-// const trail_id = path.slice(7)
+// Slice pathname string to just include the trail_id
+const trip_id = tripPath.slice(6)
+console.log(trip_id)
+
 
 // function initMap() {
 //     $.get(`/json/latlongbyid/${trail_id}`, (res) => {
